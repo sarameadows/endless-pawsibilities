3fdb75f3124348e50af2997e491046052a0a3e0b
Finn Stitcher <foundfootage@protonmail.com>
2022-08-10 13:08:56 -0700

    model index

---
+++ models/index.js
@@ -0,0 +1,11 @@
+const Animal = require('./Animal');
+const Employee = require('./Employee');
+const Foster = require('./Foster');
+
+// relationships go here
+
+module.exports = {
+    Animal,
+    Employee,
+    Foster
+};
