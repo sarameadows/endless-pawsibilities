4eadd013f2d0a2db2ab8169620799e5ab0080b1f
Finn Stitcher <foundfootage@protonmail.com>
2022-08-11 18:17:30 -0700

    fixed typos, seeds

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO animal (name, species, age, weight, special_needs) VALUES
+('Cheesemonger', 'dog', 4, 30, 0),
+('Sophia', 'dog', 16, 40, 1),
+('Ollie', 'dog', 8, 9, 0),
+('Taffeta', 'cat', 15, 10, 1),
+('Mittens', 'cat', 3, 9, 1),
+('Mr. Whiskers', 'cat', 10, 12, 0),
+('Hyde', 'rabbit', 3, 34, 0),
+('Ellie', 'small pet', 3, 0.05, 0);
