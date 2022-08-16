1e2bcee7cf2ecdd2c1d8213a2b39f15c774ca154
Finn Stitcher <foundfootage@protonmail.com>
2022-08-15 20:38:47 -0700

    polish

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
+const helpers = require('./utils/helpers');
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const sequelize = require('./config/connection');
@@ -19,7 +20,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 app.use(session(sess));
 
-const hbs = exphbs.create({});
+const hbs = exphbs.create({helpers});
  
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
