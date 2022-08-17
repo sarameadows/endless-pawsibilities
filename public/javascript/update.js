68f02e556e8cb1937dcf870132cc0f861bef6774
Sara <sameads9@gmail.com>
2022-08-16 19:47:24 -0500

    add animal event handler

--- public/javascript/update.js
+++ public/javascript/update.js
@@ -1,10 +1,32 @@
-//check that the js file is connected
-console.log('update.js connected');
+async function newAnimalFormHandler(event) {
+    event.preventDefault();
 
-//event listener for update form
-document.querySelector(".update-form").addEventListener('submit', function(event){
-    event.preventDefault()
-    console.log(event.target.children)
-})
-//loop over array to get all the form information
-//do a put request to update the animal record
+    const name = document.querySelector('#name-update').value;
+    const species = document.querySelector('#species-update').value;
+    const age = document.querySelector('#age-update').value;
+    const weight = document.querySelector('#weight-update').value;
+    const special_needs = document.querySelector('#special-update').checked;
+    console.log(special_needs);
+    const response = await fetch(`/api/animals`, {
+        method: 'POST',
+        body: JSON.stringify({
+            name,
+            species,
+            age,
+            weight,
+            special_needs
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    if(response.ok) {
+        document.location.reload();
+    } else {
+        alert(response.statusText);
+    }
+}
+
+//event listener for add animal form
+document.querySelector(".update-form").addEventListener('submit', newAnimalFormHandler);
